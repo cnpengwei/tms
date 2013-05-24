97013c0609e2752d3ac2de8a9a5f79f926b3a5c8
peng wei <cnpengwei@gmail.com>
2013-05-24 20:02:45 +0800

    add ajax recursively?

---
+++ ajax/reg_act.php
@@ -0,0 +1,67 @@
+
+<?php
+		date_default_timezone_set (PRC);//PRC  Asia/Shanghai
+		//STR_TO_DATE('" + str_dt_timerev + "', '%Y%m%d%k%i%s') 
+		//DATE_FORMAT( '1997-3-04 8:23:4', '%Y%m%d%H%i%s' )
+
+
+		// day_log("0");
+		$con = mysql_connect("localhost","root","");
+		if (!$con) {
+			day_log(mysql_error());
+			die('Could not connect: ' . mysql_error());
+		}
+
+		mysql_select_db("tms", $con);
+		// day_log("1");
+		$email = $_SERVER["QUERY_STRING"]; //$_REQUEST["email"]; //$_GET["age"] $_REQUEST["name"]
+		day_log($email);
+
+		// $paswd = '$_POST[paswd]';
+		// $uname = md5('$_POST[uname]';
+		$sql = "INSERT INTO tb_common_member (email, password, username) VALUES ('a', 'b', 'c')";
+		// $sql = "INSERT INTO tb_common_member (email, password, username) VALUES ($email, $paswd, $uname)";
+
+		if (!mysql_query($sql, $con)) {
+			day_log(mysql_error());
+			die('Error: ' . mysql_error());
+		}
+		
+		mysql_close($con);		
+
+		function day_log($msg){
+				$time_stmp = date('[Y-m-d H:i:s]');
+				$date_stmp = date('Y-m-d');
+				file_put_contents($date_stmp.".log", $time_stmp.$msg."\r\n", FILE_APPEND);
+		}
+
+
+
+
+		//获取域名或主机地址 
+		// echo $_SERVER['HTTP_HOST']."<br>"; #localhost
+
+		//获取网页地址 
+		// echo $_SERVER['PHP_SELF']."<br>"; #/blog/testurl.php
+
+
+		//获取网址参数 
+		//echo $_SERVER["QUERY_STRING"]."<br>"; #id=5 //email=[object%20Object]&paswd=[object%20Object]&uname=[object%20Object]
+
+		//获取用户代理 
+		//echo $_SERVER['HTTP_REFERER']."<br>"; 
+		
+		//获取完整的url
+		// echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		// echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
+		#http://localhost/blog/testurl.php?id=5
+
+		//包含端口号的完整url
+		// echo 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"]; 
+		#http://localhost:80/blog/testurl.php?id=5
+
+		//只取路径
+		// $url='http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"]; 
+		// echo dirname($url);
+		
+?>
