4df9b0446b39bdec62d5e803c36e86e73893f7e6
peng <cnpengwei@gmail.com>
2013-05-29 12:50:17 +0800

    'index'

--- view/index.php
+++ view/index.php
@@ -6,24 +6,43 @@
 	<style type="text/css">
 	body{
 		background-color: #eee;
+		/*text-align: center;*/
 		/*float: center;*/
 	}
+	/*
 	div#wrap{ 
-		width: 760px;
-		margin:0 auto;
+		width: 960px;
+		margin: 0 auto;
+		top:50%;
+		
 		border: 1px solid #333;
 		background-color: #ccc;
-	} 
+	}
+	*/
+	div#container{ 
+		/*background:#ccc url(stats_name.jpg) repeat-y center; */
+		position:absolute; 
+		left:50%; 
+		top:50%; 
+		width:960px;
+		/*height:300px;  */
+		margin-left:-480px; 
+
+		border: 1px solid #333;
+		background-color: #ccc;
+	}
 	</style>
 </head>
 <body>
 
 
-<div id="wrap">
+<div id="container">
+用户名: <input type="textbox" id="txt_name" /><br/>
+密&nbsp;&nbsp;&nbsp;&nbsp;码: <input type="textbox" id="txt_upwd" /><br/>
+<input type="button" id="btn_login" name="btn_login" value="登录" /> &nbsp;&nbsp; 
+<a href="#" id="fetch_upwd">忘记密码</a>
 
-	水平居中
-
-</div>
+	</div>
 
 
 
