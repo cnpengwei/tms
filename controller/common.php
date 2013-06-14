50061b5406f04b7a1d050ef664db9297474916af
peng <cnpengwei@gmail.com>
2013-06-14 17:46:35 +0800

    rm renamed files

--- controller/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	function day_log($msg){
-			$time_stmp = date('[Y-m-d H:i:s]');
-			$date_stmp = date('Y-m-d');
-			file_put_contents($date_stmp.".log", $time_stmp.$msg."\r\n", FILE_APPEND);
-	}
--- view/exmTeacher/SingleChoice/deleSingleChoiceView.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo "deleSingleChoiceView.php";
